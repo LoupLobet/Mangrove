dfe3e5eb8ee6a573a16b613b3e42f3160df2bc7f
LoupLobet <louplobet@gmail.com>
2020-06-25 23:00:09 +0200

    Makefile config

---
+++ config.mk
@@ -0,0 +1,14 @@
+# mangrove version
+VERSION = 0.1
+
+# compiler
+CC = cc
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+
+# flags
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\"
+CFLAGS = -std=c99 -pedantic -Wall -Wno-depreciated-declarations -Os ${CPPFLAGS}
+
