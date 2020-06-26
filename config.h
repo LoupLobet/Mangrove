d721a4b7201a1cdb1fbfb05490683b1b669c218a
LoupLobet <louplobet@gmail.com>
2020-06-26 15:38:54 +0200

    mangrove config file

--- config.h
+++ config.h
@@ -1,4 +1,11 @@
+/*
+ * mangrove confiuration file 
+ */
+
 unsigned int maxtreesnumber = 256;
 unsigned int maxkinshipsnumber = 256;
-char rootdir[]  = "/tmp/mangrovetree";
-char *linksymbol = "\t";
+
+/* set the mangrove trees location */
+char rootdir[]  = "~/.mangrovetree";
+/* set the link symbol/format into tree files */
+char *linksymbol = "\t-> ";
