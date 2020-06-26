e6c5b8cfa27d805b3ae62cab7d84bde78c5e8737
LoupLobet <louplobet@gmail.com>
2020-06-27 00:01:16 +0200

    Makefile configuration

--- config.mk
+++ config.mk
@@ -9,7 +9,10 @@ PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
 # flags
-LIBS = -lbsd
+
+# If you are using BSD comment next line and `#include bsd/unistd.h` in mangrove.c
+LINUX = -lbsd
+
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\"
-CFLAGS = -std=c99 -pedantic -Wall -Wno-depreciated-declarations -Os ${CPPFLAGS} ${LIBS}
+CFLAGS = -std=c99 -pedantic -Wall -Os ${CPPFLAGS} ${LINUX}
 
