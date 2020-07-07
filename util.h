7fa5222d9183c89e060c18e66a7c438f83fbd6f1
LoupLobet <louplobet@gmail.com>
2020-07-08 01:08:55 +0200

    revised for 1.0

--- util.h
+++ util.h
@@ -1,12 +1,18 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
+/* 
+ * manage bsd fonctionality for linux systems, such as
+ * getprogname() or __dead prefix for dead functions 
+ */
 #ifdef __linux__
     #include <bsd/stdlib.h>    		     /* getprogname() for usage() */
     #define __dead __attribute__((noreturn)) /* provide __dead functions */
 #endif
 
-/* arbitrary constants */
+/* 
+ * arbitrary constants
+ */
 #define MAX_LINKS_NUMBER 256
 #define MAX_TREES_NUMBER 256
 #define MAX_LINE_LENGTH  256
