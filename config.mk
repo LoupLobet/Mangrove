a06839b3902fca92afdf3c679952e222cf94ac79
LoupLobet <louplobet@gmail.com>
2020-07-08 01:07:32 +0200

    Makefile config file

--- config.mk
+++ config.mk
@@ -2,17 +2,17 @@
 VERSION = 0.1.3
 
 # compiler
-CC = gcc
+# gcc clang and tcc have been tested and seem to work for compilation
+CC = cc
 
 # paths
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-# flags
-
 # If you are using BSD comment next line 
 LINUX = -lbsd
 
+# flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\"
 CFLAGS = -std=c99 -pedantic -Wall -Os ${CPPFLAGS} ${LINUX}
 
