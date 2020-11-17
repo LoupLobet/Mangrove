0cd277856ce183e0135d08d2786a44e0b614ce97
escarg0tic <escarg0tic@puffer.fish>
2020-11-17 23:12:49 +0100

    remove useless old #includes

--- util.c
+++ util.c
@@ -1,11 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 #include <errno.h>
 #include <err.h>
-#include <limits.h>
-#include <stdarg.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "util.h"
 
