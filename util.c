468bb6d0c3f04b614f6574af68e43f1913a1763a
escarg0tic <escarg0tic@puffer.fish>
2020-11-17 22:52:57 +0100

    remove useless functions: die & estrtonum

--- util.c
+++ util.c
@@ -1,5 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 #include <errno.h>
+#include <err.h>
 #include <limits.h>
 #include <stdarg.h>
 #include <stdio.h>
@@ -8,40 +9,12 @@
 
 #include "util.h"
 
-__dead void die(const char *f, ...)
-{
-	va_list ap;
-
-	va_start(ap, f);
-	vfprintf(stderr, f, ap);
-	va_end(ap);
-	if (f[0] && f[strlen(f) - 1] == ':') {
-		fputc(' ', stderr);
-		perror(NULL);
-	} else {
-		fputc('\n', stderr);
-	}
-	exit(1);
-}
-
 void *
 ecalloc(size_t nmemb, size_t size)
 {
 	void *p;
 
 	if (!(p = calloc(nmemb, size)))
-		die("calloc:");
+		err(1, NULL);
 	return p;
-}
-
-int
-estrtonum(char *string)
-{
-	int castedint;
-	const char *errstr;
-	
-	castedint = strtonum(string, INT_MIN, INT_MAX, &errstr);
-	if (errstr != NULL)
-		die("mang: illegal integer: %s", string);
-	return castedint;
 }
--- util.h
+++ util.h
@@ -1,8 +1,6 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
-__dead void	die(const char *, ...);
 void		*ecalloc(size_t nmemb, size_t size);
-int		estrtonum(char *string);
 
 #endif
